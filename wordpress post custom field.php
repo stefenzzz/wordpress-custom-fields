7bf26ad271f5bc476c88db984d3febe0ab381da3
stefenzzz <100253086+stefenzzz@users.noreply.github.com>
2023-03-03 05:41:36 +0800

    Update wordpress post custom field.php

--- wordpress post custom field.php
+++ wordpress post custom field.php
@@ -1,5 +1,6 @@
 <?php
 
+
 // add rm events
 function rmevents_post_types(){
     register_post_type('event',[
@@ -32,6 +33,32 @@ function rm_add_custom_meta_box() {
         'side',                  // location of the input box
         'low'                     // $priority
     );
+    add_meta_box(
+        'custom_event_until_date_metabox',       // $id
+        'Event Until Date',                  // $title
+        'show_custom_event_until_meta_box',  // $callback
+        'event',                 // $page
+        'side',                  // location of the input box
+        'low'                     // $priority
+    );
+    add_meta_box(
+        'custom_event_location_metabox',       // $id
+        'Event Location',                  // $title
+        'show_custom_location_meta_box',  // $callback
+        'event',                 // $page
+        'side',                  // location of the input box
+        'low'                     // $priority
+    );
+
+    // add meta box for youtube id
+    add_meta_box(
+        'custom_youtube_id_metabox',       // $id
+        'Youtube ID',                  // $title
+        'show_custom_youtube_id_meta_box',  // $callback
+        'video',                 // $page
+        'side',                  // location of the input box
+        'low'                     // $priority
+    );
  }
  add_action('add_meta_boxes', 'rm_add_custom_meta_box');
 
@@ -40,7 +67,7 @@ function rm_add_custom_meta_box() {
     global $post;
 
     // Use nonce for verification to secure data sending
-    wp_nonce_field( basename( __FILE__ ), 'readersmagnet_our_nonce' );
+    wp_nonce_field( basename( __FILE__ ), 'readersmagnet_eventDate_nonce' );
 
 
     $custom = get_post_custom( $post->ID );
@@ -54,6 +81,62 @@ function rm_add_custom_meta_box() {
 
     <?php
 }
+function show_custom_event_until_meta_box(){
+    global $post;
+    
+    // Use nonce for verification to secure data sending
+    wp_nonce_field( basename( __FILE__ ), 'readersmagnet_eventUntil_nonce' );
+
+
+    $custom = get_post_custom( $post->ID );
+    $rm_post_event_until_date = $custom[ "_event_until_date" ][0] ?? '';
+
+  
+    ?>
+    
+    <!-- my custom value input -->
+    <input type="date" name="_event_until_date" value="<?=$rm_post_event_until_date?>" placeholder= "Event ID">
+
+    <?php
+    
+}
+function show_custom_location_meta_box(){
+    global $post;
+    
+    // Use nonce for verification to secure data sending
+    wp_nonce_field( basename( __FILE__ ), 'readersmagnet_eventLocation_nonce' );
+
+
+    $custom = get_post_custom( $post->ID );
+    $rm_post_location_date = $custom[ "_event_location" ][0] ?? '';
+
+  
+    ?>
+    
+    <!-- my custom value input -->
+    <input type="text" name="_event_location" value="<?=$rm_post_location_date?>" placeholder= "Event Location">
+
+    <?php
+
+}
+function show_custom_youtube_id_meta_box(){
+    global $post;
+
+    // Use nonce for verification to secure data sending
+    wp_nonce_field( basename( __FILE__ ), 'readersmagnet_youtubeID_nonce' );
+
+
+    $custom = get_post_custom( $post->ID );
+    $rm_post_location_date = $custom[ "_rm_youtube_id" ][0] ?? '';
+
+  
+    ?>
+    
+    <!-- my custom value input -->
+    <input type="text" name="_rm_youtube_id" value="<?=$rm_post_location_date?>" placeholder= "Youtube ID">
+
+    <?php
+}
 
 
  // save field value
@@ -63,14 +146,48 @@ function save_post_meta_boxes(){
         return;
     }
 
-    if (!isset($_POST['readersmagnet_our_nonce']) || !wp_verify_nonce($_POST['readersmagnet_our_nonce'], basename(__FILE__))){
-        return 'nonce not verified';
+    if (isset($_POST['readersmagnet_eventDate_nonce']) ){
+
+        
+        if(wp_verify_nonce($_POST['readersmagnet_eventDate_nonce'], basename(__FILE__)))
+        {
+            if($post)
+            {
+                update_post_meta( $post->ID, "_event_date", sanitize_text_field( $_POST[ "_event_date" ] ) );
+            }
+        }
+
+    }
+    if (isset($_POST['readersmagnet_eventUntil_nonce'])){
+
+        if(wp_verify_nonce($_POST['readersmagnet_eventUntil_nonce'], basename(__FILE__))){
+            if($post)
+            {
+                update_post_meta( $post->ID, "_event_until_date", sanitize_text_field( $_POST[ "_event_until_date" ] ) );
+            }
+        }
+
+    }
+    if (isset($_POST['readersmagnet_eventLocation_nonce'])){
+        if(wp_verify_nonce($_POST['readersmagnet_eventLocation_nonce'], basename(__FILE__))){
+            if($post)
+            {
+                update_post_meta( $post->ID, "_event_location", sanitize_text_field( $_POST[ "_event_location" ] ) );
+            }
+        }
+      
+    }
+    if (isset($_POST['readersmagnet_youtubeID_nonce'])){
+        if(wp_verify_nonce($_POST['readersmagnet_youtubeID_nonce'], basename(__FILE__))){
+            if($post)
+            {
+                update_post_meta( $post->ID, "_rm_youtube_id", sanitize_text_field( $_POST[ "_rm_youtube_id" ] ) );
+            }
+        }
+      
     }
    
-    if($post)
-    {
-        update_post_meta( $post->ID, "_event_date", sanitize_text_field( $_POST[ "_event_date" ] ) );
-    }
+  
     
 }
 
@@ -79,34 +196,83 @@ add_action( 'save_post', 'save_post_meta_boxes' );
 
 
 
-add_shortcode('events-list', 'rm_events');
+add_shortcode('events-list', 'rm_upcoming_events');
 
-function rm_events(){
+function rm_upcoming_events(){
     global $post;
 
     $args = array(
         'post_type'=>'event', 
         'post_status'=>'publish', 
-        'posts_per_page'=>10, 
+        'posts_per_page'=>3, 
         'orderby'=>'meta_value',
         'meta_key' => '_event_date',
-        'order'=>'DESC'
+        'order'=>'ASC'
     );
 
     $query = new WP_Query($args);
-    $content = '<ul>';
+    $content = '<div class="events-container">';
 
     if($query->have_posts()):
 		while($query->have_posts()): $query->the_post();
-        $content .= '<li><a href="'.get_the_permalink().'">'. get_the_title() .'</a> - '.date_format(date_create(get_post_meta($post->ID, '_event_date', true)), 'jS F').'</li>';
+        $even_date = date_format(date_create(get_post_meta($post->ID, '_event_date', true)), 'F d, Y');
+        $even_until_date = date_format(date_create(get_post_meta($post->ID, '_event_until_date', true)), 'F d, Y');
+        $date = $even_date.' - '.$even_until_date;
+        if($even_date === $even_until_date ){
+            $date = $even_date;
+        }
+       
+        
+        $content .= '<div class="event-post-container">
+        <div class="event-image-container">
+        <img src="'.get_the_post_thumbnail_url().'">
+        </div>
+        <div class="event-content-container">
+          <h3><a class="event-post-title" href="'.get_the_permalink().'">'.get_the_title().'</a></h3>
+          <span class="event-date">Date: '.$date.'</span>
+           <span class="event-Location">Location: '.get_post_meta($post->ID, '_event_location', true).'</span>
+                  <div class="button-wrapper">
+              <a href="'.get_the_permalink().'" class="event-view-more">View More</a>
+              </div>  
+        </div>
+      </div>';
     endwhile;
+    wp_reset_postdata();
     else: 
-        _e('Sorry, nothing to display.', 'vicodemedia');
+        _e('Sorry, nothing to display.', 'RMevents');
     endif;
-    $content .= '</ul>';
+    $content .= '</div>';
   return $content;
 
 
 
   
 }
+
+<?php
+
+
+
+function rmvideos_post_types(){
+    register_post_type('video',[
+        'public' => true,
+        'labels' =>[
+            'name' => 'Videos',
+            'add_new_item' => 'Add New Video',
+            'edit_item' => 'Edit Video',
+            'all_items' => 'All Videos',
+            'singular_name' => 'Video'
+        ],
+        'menu_icon' => 'dashicons-videos',
+        'supports' => ['title','editor','thumbnail','revisions','custom-fields'],
+        'has_archive' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'can_export' => true,
+        'has_archive' => true
+    ]);
+}
+
+
+add_action('init','rmvideos_post_types');
+
